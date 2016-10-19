7765ceb967e3a7542e24681488740acdf75d3407
Desiree Conceicao <dconceicao@atlassian.com>
2016-10-19 18:19:28 +1100

    Website changes
    added website files

---
+++ Desnet/js/custom.js
@@ -0,0 +1,47 @@
+var resizeFunction = function(){
+	var anchors = document.getElementsByClassName('change')
+  	if(window.innerWidth < 590){
+		anchors[0].innerHTML = 'Me'
+		anchors[1].innerHTML = 'Lists'
+		anchors[2].innerHTML = 'Faves'
+		anchors[3].innerHTML = 'Contact'
+  	}
+	else {     
+		anchors[0].innerHTML = 'A little more about me'
+		anchors[1].innerHTML = 'Bucket List'
+		anchors[2].innerHTML = 'Favourite things'
+		anchors[3].innerHTML = 'Get in contact'
+	}
+}
+  
+window.addEventListener('resize', resizeFunction)
+
+$(document).ready(function(){
+	resizeFunction()
+	$('.imagecarousel').slick({
+		dots: true,
+		infinite: true,
+		speed: 300,
+		slidesToShow: 1,
+		centerMode: true,
+		variableWidth: true
+	});
+	$(window).scroll(function() {
+    	if ($(this).scrollTop() >= 100) {
+        	$('#top').fadeIn("fast");       
+    	} 
+    	else {
+        	$('#top').fadeOut("fast");      
+    	}
+	});
+	$('#top').click(function() {            
+    	$('body,html').animate({
+        	scrollTop : 0                   
+    	}, 500);
+	});
+});
+
+
+
+// - Add jquery arrow to take you to 'next' section (MAYBE)
+// defs want to add smooth scrolling
