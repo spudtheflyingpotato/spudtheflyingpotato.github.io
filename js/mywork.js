4836cc0431ca08423a64359b3911925b5f9947e9
Desiree Conceicao <dconceicao@atlassian.com>
2016-10-25 21:29:55 +1100

    Format code
    Just formatting my code

--- js/mywork.js
+++ js/mywork.js
@@ -1,46 +1,46 @@
 var resizePageMenuText = function () {
-	var anchors = $('.change')
-	if (window.innerWidth < 590) {
-		anchors.eq(0).html('Docs');
-		anchors.eq(1).html('Articles');
-		anchors.eq(2).html('Misc');
-		anchors.eq(3).html('Contact');
-	}
-	else {
-		anchors.eq(0).html('Documentation');
-		anchors.eq(1).html('Articles and Reviews');
-		anchors.eq(2).html('Miscellaneous Things');
-		anchors.eq(3).html('Get in contact');
-	}
+    var anchors = $('.change')
+    if (window.innerWidth < 590) {
+        anchors.eq(0).html('Docs');
+        anchors.eq(1).html('Articles');
+        anchors.eq(2).html('Misc');
+        anchors.eq(3).html('Contact');
+    }
+    else {
+        anchors.eq(0).html('Documentation');
+        anchors.eq(1).html('Articles and Reviews');
+        anchors.eq(2).html('Miscellaneous Things');
+        anchors.eq(3).html('Get in contact');
+    }
 }
 
 // Changes text on page resize
 window.addEventListener('resize', resizePageMenuText)
 
 var elevatorScroll = function () {
-	({
-		element: $('.elevator').get(0),
-		mainAudio: '../audio/elevator.mp3',
-		endAudio: '../audio/ding.mp3'
-	});
+    ({
+        element: $('.elevator').get(0),
+        mainAudio: '../audio/elevator.mp3',
+        endAudio: '../audio/ding.mp3'
+    });
 }
 
 var smoothScroll = function () {
-	$.localScroll({ filter: '.smoothScroll' });
+    $.localScroll({ filter: '.smoothScroll' });
 }
 
 var divLinker = function () {
 	/* Searches for an <a> tag inside the div and navigates 
 	to the href of the <a> tag when the div is clicked */
-	$(".linkDiv").click(function () {
-		window.location = $(this).find("a").attr("href");
-		return false;
-	});
+    $(".linkDiv").click(function () {
+        window.location = $(this).find("a").attr("href");
+        return false;
+    });
 }
 
 $(document).ready(function () {
-	resizePageMenuText();
-	elevatorScroll();
-	smoothScroll();
-	divLinker();
+    resizePageMenuText();
+    elevatorScroll();
+    smoothScroll();
+    divLinker();
 });
